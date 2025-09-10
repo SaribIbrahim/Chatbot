0b41af4319b018d5e6147415e671a998499460e9
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-09-10 17:00:17 +0500

    Navbar and homepage component made

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import MainPage from "./components/MainPage";
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="grid grid-cols-5 h-screen text-white">
+      <Sidebar />
+      <MainPage />
+    </div>
+  );
 }
 
-export default App
+export default App;
